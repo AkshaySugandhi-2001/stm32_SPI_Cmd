72c94c578fce29b4bd7bdf81e19bbf83e23ab090
unknown <akshaysugandhi10788@gmail.com>
2024-08-10 19:29:10 +0530

    first

---
+++ Debug/Drivers/src/stm32f407xx_spi_driver.d
@@ -0,0 +1,10 @@
+Drivers/src/stm32f407xx_spi_driver.o: \
+ ../Drivers/src/stm32f407xx_spi_driver.c \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h:
---
+++ Debug/Src/main.d
@@ -0,0 +1,8 @@
+Src/main.o: ../Src/main.c D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h \
+ D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx.h:
+D:/st/SPI_Cmd/Drivers/Inc/stm32f407xx_spi_driver.h:
